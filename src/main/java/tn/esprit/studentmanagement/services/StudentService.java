6bf9e0ecfd7070e769a69ad61b048f22a698fcc0
Mariem BenYounes <maryemby21@gmail.com>
2025-10-11 10:00:38 +0100

    feat: added msg in service

--- src/main/java/tn/esprit/studentmanagement/services/StudentService.java
+++ src/main/java/tn/esprit/studentmanagement/services/StudentService.java
@@ -16,5 +16,8 @@ public class StudentService implements IStudentService {
     public Student getStudentById(Long id) { return studentRepository.findById(id).orElse(null); }
     public Student saveStudent(Student student) { return studentRepository.save(student); }
     public void deleteStudent(Long id) { studentRepository.deleteById(id); }
+    public void showMessage() {
+        System.out.println("Hello from StudentService");
+    }
 
 }
