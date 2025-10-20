c592f5b590e770fe6df562cc0d7fd1a2f494d504
Haithem Farjallah <haithemfarjallah2002@gmail.com>
2025-10-20 22:55:08 +0100

    feat: add log messages to enrollement service

--- src/main/java/tn/esprit/studentmanagement/services/EnrollmentService.java
+++ src/main/java/tn/esprit/studentmanagement/services/EnrollmentService.java
@@ -1,6 +1,7 @@
 package tn.esprit.studentmanagement.services;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tn.esprit.studentmanagement.repositories.EnrollmentRepository;
@@ -8,27 +9,32 @@ import tn.esprit.studentmanagement.entities.Enrollment;
 import java.util.List;
 
 @Service
+@Slf4j
 public class EnrollmentService implements IEnrollment {
     @Autowired
     EnrollmentRepository enrollmentRepository;
 
     @Override
     public List<Enrollment> getAllEnrollments() {
+        log.info("getAllEnrollments is being called by Haithem ");
         return enrollmentRepository.findAll();
     }
 
     @Override
     public Enrollment getEnrollmentById(Long idEnrollment) {
+        log.info("getEnrollmentById is being called by Haithem ");
         return enrollmentRepository.findById(idEnrollment).get();
     }
 
     @Override
     public Enrollment saveEnrollment(Enrollment enrollment) {
+        log.info("saveEnrollment is being called by Haithem ");
         return enrollmentRepository.save(enrollment);
     }
 
     @Override
     public void deleteEnrollment(Long idEnrollment) {
-enrollmentRepository.deleteById(idEnrollment);
+        log.info("deleteEnrollment is being called by Haithem ");
+        enrollmentRepository.deleteById(idEnrollment);
     }
 }
